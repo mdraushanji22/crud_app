334c36f4a969712352c31537d964660f8984a913
mdraushan <mdraushanji22@gmail.com>
2026-02-11 16:31:12 +0530

    CRUD-APPLICATION

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+</head>
+
+<body>
+    <h1>CRUD APPLICATION IN PHP</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Age</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th>3</th>
+                <th>Vikesh</th>
+                <th>Kumar</th>
+                <th>25</th>
+            </tr>
+            <tr>
+                <th>4</th>
+                <th>Nabeel</th>
+                <th>Khan</th>
+                <th>7</th>
+            </tr>
+        </tbody>
+    </table>
+</body>
+
+</html>
